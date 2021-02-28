b5f0dace96735a3853ca5ea02f94b509d3d82066
Farukhrana14 <farukhrana14@gmail.com>
2021-03-01 02:28:03 +0600

    images and data displayed

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -3,11 +3,28 @@ import './Player.css'
 
 
 const Player = (props) => {
-    const{allRounderRating, battingStyle, country, fee_mln_rupee, img, name} = props.player;
+    const { allRounderRating, battingStyle, country, fee_mln_rupee, img, name } = props.player;
     return (
-        <div>
-            <h1>This is Player:{name}</h1>
+        <div className='player-container'>
+          <div className='single-player'>
+            <img src={img} alt=""/>
+            <h3>Name: {name}</h3>
+            <hr/>
+            <h4>Player Statistics</h4>
+            <ul>
+              <li>All Rounder Rating: {allRounderRating}</li>
+              <li>Batting style: {battingStyle}</li>
+              <li>Country: {country}</li>
+              <li>Fee in million USD: {fee_mln_rupee}</li>
+        
+            </ul>
+            <hr/>
+            <button>Add</button> <span></span>
+            <button>Remove</button>
+
+          </div>
         </div>
+            
     );
 };
 
